ce66d6979324f8fb7613f3cb38ea605e967354b2
Sam Slover <samjslover@gmail.com>
2013-10-16 00:25:15 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,205 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+  , io = require('socket.io')
+  , http = require('http')
+  , twitter = require('ntwitter')
+  , cronJob = require('cron').CronJob
+  , _ = require('underscore')
+  , path = require('path');
+
+//Create an express app
+var app = express();
+
+//Create the HTTP server with the express app as an argument
+var server = http.createServer(app);
+
+
+// Twitter foods array. We want a list for allFoods, and then we will check that list against the healthy and unhealthy foods list
+var allFoods = ['salad', 'salmon', 'tofu', 'vegetables', 'avocado', 'broccoli', 'spinach', 'sweet potatoes', 'berries', 'dark chocolate', 'hamburger', 'pizza', 'french fries', 'fried chicken', 'hot dog', 'cinnamon roll', 'ice cream', 'soda', 'bbq', 'candy'];
+var healthyFoods =  ['salad', 'salmon', 'tofu', 'vegetables', 'avocado', 'broccoli', 'spinach', 'sweet potatoes', 'berries', 'dark chocolate'];
+var unhealthyFoods = ['hamburger', 'pizza', 'french fries', 'fried chicken', 'hot dog', 'cinnamon roll', 'ice cream', 'soda', 'bbq', 'candy'];
+
+//These object structures keep track of the total number of tweets for each category, and a map of all the foods and how many tweets they've each gotten
+var healthyList = {
+    total: 0,
+    foods: {}
+};
+
+var unhealthyList = {
+    total: 0,
+    foods: {}
+};
+
+var currentTweet;
+var currentPhotoURL;
+var currentTweetURL;
+
+//Set the food lists to zero.
+_.each(healthyFoods, function(v) { healthyList.foods[v] = 0; });
+_.each(unhealthyFoods, function(v) { unhealthyList.foods[v] = 0; });
+
+
+//Generic Express setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+// setup template engine - we're using Hogan-Express
+app.set('view engine', 'html');
+app.set('layout','layout');
+app.engine('html', require('hogan-express')); // https://github.com/vol4ok/hogan-express
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//We're using bower components so add it to the path to make things easier
+app.use('/components', express.static(path.join(__dirname, 'components')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// the dataObject we are sending to the page, includes all the data we need
+var dataObject = {
+  healthData: healthyList, 
+  unhealthData: unhealthyList,
+  tweet: []
+};
+
+//Our only route! Render it with the current healthyList and unhealthyList
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+//Start a Socket.IO listen
+var sockets = io.listen(server);
+
+//Set the sockets.io configuration.
+//THIS IS NECESSARY ONLY FOR HEROKU!
+sockets.configure(function() {
+  sockets.set('transports', ['xhr-polling']);
+  sockets.set('polling duration', 10);
+});
+
+//If the client just connected, give them fresh data!
+sockets.sockets.on('connection', function(socket) { 
+
+    //Reset the total
+    healthyList.total = 0;
+    unhealthyList.total = 0;
+
+    //Clear out everything in the map
+    _.each(healthyFoods, function(v) { healthyList.foods[v] = 0; });
+    _.each(unhealthyFoods, function(v) { unhealthyList.foods[v] = 0; });
+
+    socket.emit('connection', dataObject);
+});
+
+//Set up the twitter component
+
+var t = new twitter({
+      consumer_key: 'HERLR3SfVGutshK5Nkew', 
+      consumer_secret: 'bTRnn8EFxCMC1k8mPzGyJzceC1wlBawlJv7sZ0jQHE',
+      access_token_key: '81014526-0wOYZQPGHbUyJPzPGDv9jI5ZKzWY4BVfaR9I4rBqw',
+      access_token_secret: 'FDq4Iokv3qiuBiw1lfEQwnV5VwwnrABOhHnXjN1BSAU'
+});
+
+//Tell the twitter API to filter on the allFoods, and then we will see if the foods are in our healthy or unhealthy list
+// Here, we are filtering the location to just tweets in the United States
+t.stream('statuses/filter', { track: allFoods}, function(stream) {
+
+  //We have a connection. Now watch the 'data' event for incomming tweets.
+  stream.on('data', function(tweet) {
+
+    //These variables are used to indicate whether a food was actually mentioned.
+    var healthyClaimed = false;
+    var unhealthyClaimed = false;
+
+    //Make sure it was a valid tweet
+    if (tweet.text !== undefined) {
+
+      //We're gunna do some indexOf comparisons and we want it to be case agnostic.
+      var text = tweet.text.toLowerCase();
+
+      // we need to know whether or not there is some indication they ate the food or wanted it. 
+      // So let's only use tweets that some key words that show desire: eat, want, delicious, lunch, breakfast, dinner, snack
+      var ateIt = false;
+      if (text.indexOf('delicious') !== -1 || text.indexOf('eat') !== -1 || text.indexOf('want') !== -1 || text.indexOf('lunch') !== -1 || text.indexOf('breakfast') !== -1 || text.indexOf('dinner') !== -1 || text.indexOf('snack') !== -1){
+         ateIt = true;
+      }
+
+      //Go through every health and then unhealthy food and see if it was mentioned. If so, increment its counter and
+      //set its 'claimed' variable to true to indicate something was mentioned so we can increment
+      //the 'total' counter!
+      _.each(healthyFoods, function(v) {
+          if (text.indexOf(v.toLowerCase()) !== -1 && ateIt) {
+                    console.log("!!!!they ate it healthy!!!!" + text);
+                    healthyList.foods[v]++;
+                    healthyClaimed = true;
+                    dataObject.tweet.push(tweet.text);
+                    dataObject.tweet.push(tweet.user.profile_image_url);
+                    dataObject.tweet.push("https://twitter.com/" + tweet.user.screen_name +"/status/" + tweet.id_str);
+                    dataObject.tweet.push(v);
+                    dataObject.tweet.push(healthyList.foods[v]);
+              }
+      });
+
+      _.each(unhealthyFoods, function(v) {
+          if (text.indexOf(v.toLowerCase()) !== -1 && ateIt) {
+                    console.log("!!!!they ate it unhealthy!!!!" + text);
+                    unhealthyList.foods[v]++;
+                    console.log(v);
+                    unhealthyClaimed = true;
+                    dataObject.tweet.push(tweet.text);
+                    dataObject.tweet.push(tweet.user.profile_image_url);
+                    dataObject.tweet.push("https://twitter.com/" + tweet.user.screen_name +"/status/" + tweet.id_str);
+                    dataObject.tweet.push(v);
+                    dataObject.tweet.push(unhealthyList.foods[v]);
+              }
+
+      });
+
+      //If something was mentioned, increment the total counter and send the update to all the clients
+      if (healthyClaimed) {
+          //Increment total
+          healthyList.total++;
+          //Send to all the clients
+          sockets.sockets.emit('data', dataObject);
+          dataObject.tweet.length = 0; 
+      }
+
+      if (unhealthyClaimed) {
+          //Increment total
+          unhealthyList.total++;
+          //Send to all the clients
+          sockets.sockets.emit('data', dataObject);
+          dataObject.tweet.length = 0; 
+
+      }
+    }
+  });
+});
+
+// //Reset everything on a new day!
+// new cronJob('0 0 0 * * *', function(){
+//     //Reset the total
+//     healthyList.total = 0;
+//     unhealthyList.total = 0;
+
+//     //Clear out everything in the map
+//     _.each(healthyFoods, function(v) { healthyList.foods[v] = 0; });
+//     _.each(unhealthyFoods, function(v) { unhealthyList.foods[v] = 0; });
+
+//     //Send the update to the clients
+//     socket.emit('data', dataObject);
+
+// }, null, true);
+
+//Create the server
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/javascripts/script.js
@@ -0,0 +1,90 @@
+$(function() {
+    var socket = io.connect(window.location.hostname);
+
+    var startingHealthTotal;
+    var startingUnhealthTotal;
+    var startingTotal;
+
+    socket.on('connect', function() {
+        console.log("Connected");
+    });
+
+    // Receive a message
+    socket.on('connection', function(data) {
+        startingHealthTotal = data.healthData.total;
+        startingUnhealthTotal = data.unhealthData.total;
+        startingTotal = data.healthData.total + data.unhealthData.total;
+    });
+
+
+    socket.on('data', function(data) {
+
+        var healthTotal = data.healthData.total - startingHealthTotal;
+        var unhealthTotal = data.unhealthData.total - startingUnhealthTotal;
+        var totalTweets = (data.healthData.total + data.unhealthData.total) - startingTotal;
+        var tweet = data.tweet[0];
+        var photoURL = data.tweet[1]; 
+        var tweetURL = data.tweet[2]; 
+        var food = data.tweet[3]; 
+        var foodCount = data.tweet[4]; 
+
+        console.log(food);
+        console.log(foodCount);
+        // deal with the cases where some of the foods have spaces
+        if (food === "sweet potatoes"){
+            food = "sweet-potatoes";
+        }
+
+        if (food === "dark chocolate"){
+            food = "dark-chocolate";
+        }
+
+        if (food === "french fries"){
+            food = "french-fries";
+        }
+
+        if (food === "fried chicken"){
+            food = "fried-chicken";
+        }
+
+        if (food === "hot dog"){
+            food = "hot-dog";
+        }
+
+        if (food === "cinnamon roll"){
+            food = "cinnamon-roll";
+        }
+
+        if (food === "ice cream"){
+            food = "ice-cream";
+        }
+
+    // we are going to append the current tweet to the correct place
+    // first set up the html
+    var html = '<li><a href='+tweetURL+' target=\'_blank\'><img src='+photoURL+' height=\'30\' width=\'30\'></li>';
+
+    //then append that to the correct food
+    $("#"+food).append(html);
+
+    // now let's update the number of tweets for that food            
+    $('#tweetCount-'+food).text(foodCount);
+
+    // for the top area of the page, here we do all the calculations on the total tweets and the percentage breakdown
+    $("#tweetCount").text(totalTweets);
+
+    var healthyWidth = Math.round((healthTotal / totalTweets) * 100);
+    var unhealthyWidth = Math.floor((unhealthTotal / totalTweets) * 100);        
+
+    $("#good-chart").css("width", healthyWidth +"%");
+    $("#bad-chart").css("width", unhealthyWidth +"%");
+
+    $("#goodData").text(healthyWidth + "% (" + healthTotal +")");
+    $("#badData").text(unhealthyWidth + "% (" + unhealthTotal +")");
+
+
+
+    $('#last-update').text(new Date().toTimeString());
+
+    });
+
+})
